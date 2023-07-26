7e5b98be64910eab0b2715901ca55ee9324a08ba
Estygrogan <dbobbet@gmail.com>
2023-07-26 21:03:01 +0000

    Fix form functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 //Declared variables
+import questions from "./questions.js"
 let startButton = document.getElementById("start-btn");
 let questContainerElement = document.getElementById("question-container");
 let shuffledQuestions, currentQuestionIndex;
@@ -8,238 +9,20 @@ let nextButton = document.getElementById("next-btn");
 let openInfo = document.getElementById("info-btn")
 let closeInfo = document.getElementById("close-info")
 let quizInfo = document.getElementById("quiz-info")
-let formButton = document.getElementById("form-button");
-let nameForm = document.getElementById("form-container")
+let nameForm = document.getElementById("form-container");
+let formSubmit = document.getElementById("form-submit");
 let scoreBoard = document.getElementById("score-container");
 let score = 0;
 let playerName = "";
 
 
 startButton.addEventListener("click", openForm);
-formButton.addEventListener("click", startGame);
 nextButton.addEventListener("click", () => {
     currentQuestionIndex++;
     setNextQuestion();
 });
 
 
-// List of quiz questions
-let questions = [{
-        question: "What is the name of Nintendo's most recent console",
-        answers: [{
-                text: "Switch",
-                correct: true
-            },
-            {
-                text: "3DS",
-                correct: false
-            },
-            {
-                text: "Gamecube",
-                correct: false
-            },
-            {
-                text: "Vita",
-                correct: false
-            },
-        ]
-
-    },
-    {
-        question: "What mathematician is widely considered to be the father of theoretical computer science? ",
-        answers: [{
-                text: "John von Neumann",
-                correct: false
-            },
-            {
-                text: "David Hilbert",
-                correct: false
-            },
-            {
-                text: "Alan Turing",
-                correct: true
-            },
-            {
-                text: "Georg Cantor",
-                correct: false
-            },
-        ]
-
-    },
-    {
-        question: "What PC components are Nvidia primarily known for manufacturing?",
-        answers: [{
-                text: "GPU",
-                correct: true
-            },
-            {
-                text: "CPU",
-                correct: false
-            },
-            {
-                text: "RAM",
-                correct: false
-            },
-            {
-                text: "Motherboards",
-                correct: false
-            },
-        ]
-
-    },
-    {
-        question: "What is the best selling games console of all time?",
-        answers: [{
-                text: "Game Boy",
-                correct: true
-            },
-            {
-                text: "Nintendo Switch",
-                correct: false
-            },
-            {
-                text: "Nintendo DS",
-                correct: false
-            },
-            {
-                text: "Playstation 2",
-                correct: true
-            },
-        ]
-
-    },
-    {
-        question: "Satya Nadella is the current CEO of which tech company?",
-        answers: [{
-                text: "Apple",
-                correct: false
-            },
-            {
-                text: "Sony",
-                correct: false
-            },
-            {
-                text: "Microsoft",
-                correct: true
-            },
-            {
-                text: "Amazon",
-                correct: false
-            },
-        ]
-
-    },
-    {
-        question: "What is the name of Nintendo's most recent console",
-        answers: [{
-                text: "Switch",
-                correct: true
-            },
-            {
-                text: "3DS",
-                correct: false
-            },
-            {
-                text: "Gamecube",
-                correct: false
-            },
-            {
-                text: "Vita",
-                correct: false
-            },
-        ]
-
-    },
-    {
-        question: "Which animation studio was Steve Jobs a co-founder of?",
-        answers: [{
-                text: "Illumination",
-                correct: false
-            },
-            {
-                text: "Pixar",
-                correct: true
-            },
-            {
-                text: "Dreamworks",
-                correct: false
-            },
-            {
-                text: "Cartoon Saloon",
-                correct: false
-            },
-        ]
-
-    },
-    {
-        question: "What large game publisher are Microsoft attempting to acquire?",
-        answers: [{
-                text: "Take Two",
-                correct: false
-            },
-            {
-                text: "EA",
-                correct: false
-            },
-            {
-                text: "Ubisoft",
-                correct: false
-            },
-            {
-                text: "Activision",
-                correct: true
-            },
-        ]
-
-    },
-    {
-        question: "Which of the following social media companies does Meta own?",
-        answers: [{
-                text: "Facebook",
-                correct: false
-            },
-            {
-                text: "Instagram",
-                correct: false
-            },
-            {
-                text: "WhatsApp",
-                correct: false
-            },
-            {
-                text: "All of the above",
-                correct: true
-            },
-        ]
-
-    },
-    {
-        question: "What year was Google founded?",
-        answers: [{
-                text: "1997",
-                correct: false
-            },
-            {
-                text: "1998",
-                correct: true
-            },
-            {
-                text: "1999",
-                correct: false
-            },
-            {
-                text: "2000",
-                correct: false
-            },
-        ]
-
-    },
-];
-/*
-openInfo.addEventListener("click", () => {
-    quizInfo.showModal();
-})
-*/
 
 function showInfo() {
     quizInfo.classList.remove("hide");
@@ -260,13 +43,14 @@ function openForm() {
 }
 
 
-function handleSubmit() {
-    nameForm.preventDefault();
+function handleSubmit(event) {
+    event.preventDefault();
     let username = document.getElementById("name").value
     document.getElementById("username-fill").innerHTML = username;
+    startGame()
 }
 
-formButton.addEventListener("submit", handleSubmit());
+formSubmit.addEventListener("submit", handleSubmit);
 
 //* Function to start the game and randomise order of questions
 function startGame() {
